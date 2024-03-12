9689e23df6ce927edb77a3718565b0ad6df1c378
Niva Surendran <niva.surendran@claysys.in>
2024-03-12 17:49:51 +0530

    new change

---
+++ jsDemo/Scripts/CustomJS/demoJavaScript.js
@@ -0,0 +1,12 @@
+﻿
+
+function demo()
+{
+    document.getElementById('demop').style.cssText = 'font-size: 35px; color: green;';
+   // document.getElementById('demo').style.display = 'lock';
+    
+}
+function hide() {
+
+    document.getElementById('hidden').style.display = "block";
+}
